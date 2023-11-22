4c47e5da889f151fcbc3054e1a16988f4668409c
termtate <meng360842@gmail.com>
2023-11-22 09:28:15 +0800

    .

--- src/sockets/test/di.py
+++ src/sockets/test/di.py
@@ -1,5 +1,5 @@
 from injector import singleton, provider, Module
-from sockets.manager import SocketsManager
+from sockets.manager import AttackStream, SocketsManager
 from sockets.test.fake_manager import FakeSocketsManager
 
 
@@ -8,3 +8,8 @@ class TestSocketsManagerModule(Module):
     @provider
     def provide_sockets_manager(self) -> SocketsManager:
         return FakeSocketsManager()
+
+    @singleton
+    @provider
+    def provide_attack_stream(self, sm: SocketsManager) -> AttackStream:
+        return sm.get_attack_stream()
