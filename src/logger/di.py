98bb63fc0d4978866252a4d99b1422276f3ac8ed
termtate <meng360842@gmail.com>
2023-12-13 09:43:10 +0800

    log文件 增加时间后缀

--- src/logger/di.py
+++ src/logger/di.py
@@ -9,7 +9,7 @@ class LoggerModule(Module):
     def provide_logger(self) -> Logger:
         # https://loguru.readthedocs.io/en/stable/api/logger.html#loguru._logger.Logger.add
         logger.add(
-            "log/received.log",
+            "log/received_{time}.log",
             retention="1 week",  # 保留最新一周数据
             rotation="10 MB"  # 超过10mb创建新文件
         )
