5f965b5c7513487fbf1b6bc83c079b94da0fce81
termtate <meng360842@gmail.com>
2023-12-20 21:26:41 +0800

    改了win honeypot schema类型

--- src/feature/real_honeypot/schema.py
+++ src/feature/real_honeypot/schema.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import Literal
-from pydantic import ConfigDict, BaseModel
+from pydantic import ConfigDict, BaseModel, IPvAnyAddress
 
 
 class AttackSchema(BaseModel):
@@ -10,8 +10,8 @@ class AttackSchema(BaseModel):
     protocol: Literal["ICMP", "TCP", "UDP"]
     source_port: int | None
     dest_port: int | None
-    source_ip: str
-    dest_ip: str
+    source_ip: IPvAnyAddress
+    dest_ip: IPvAnyAddress
     content: str
 
     @classmethod
