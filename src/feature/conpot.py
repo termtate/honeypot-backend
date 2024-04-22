ca56ddea65fadd7a2166d78e1f81fdcb9ebd8ddd
termtate <meng360842@gmail.com>
2024-04-22 18:27:08 +0800

    示例

--- src/feature/conpot.py
+++ src/feature/conpot.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 from .base import Honeypot, APIRouter
 from .base.mixin.docker import DockerMixin
+from fastapi_injector import Injected
+from db.crud import CRUDWithSession
 
 
 class Model(Base):
@@ -21,7 +23,7 @@ class DBModel(Model, table=True):
 
 
 class Conpot(Honeypot[Model, DBModel], DockerMixin):
-    router = APIRouter(prefix="/conpot_attacks", tags=["conpot"])
+    router = APIRouter(prefix="/conpot", tags=["conpot"])
     attack_model = Model
     db_model = DBModel
 
@@ -37,3 +39,18 @@ class Conpot(Honeypot[Model, DBModel], DockerMixin):
         route.configure_get_attacks()
         route.configure_create_attack()
         route.configure_send_attack_with_websocket()
+
+
+@Conpot.router.get("/analysis1")
+async def analysis():
+    """
+    给honeypot增加接口的示例
+    """
+    return {}
+
+
+@Conpot.router.get("/analysis2")
+async def analysis_with_data_source(
+    database: CRUDWithSession[DBModel] = Injected(Conpot.CRUD)
+):
+    return [item async for item in database.get()]
